724326b55315725ca1c30149427b52d839096570
avary-f <avaryfielding@gmail.com>
2024-10-09 21:01:02 -0600

    CHORE - init phase 3

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,13 @@
 import chess.*;
+import server.Server;
+
+import javax.sound.sampled.Port;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        var server = new Server();
+        var port = 8080;
+        server.run(port);
+        System.out.println("♕ 240 Chess Server: " + port);
     }
 }
