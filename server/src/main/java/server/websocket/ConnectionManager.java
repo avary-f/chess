359d84e0f8cd814350b69cc9158c2207c6727e67
Avary Fielding <avaryef@byu.edu>
2024-12-04 18:51:00 -0700

    WIP - Phase 6, finished

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -23,7 +23,7 @@ public class ConnectionManager {
         for(Integer gameID: connections.keySet()) {
             CopyOnWriteArrayList<Connection> list = connections.get(gameID);
             if (list == null) {
-                System.out.println("Having trouble connecting to the other client. Try logging out and back in."); // Nothing to broadcast
+                System.out.println("Having trouble connecting to the other client. Please try logging out and back in."); // Nothing to broadcast
                 return;
             }
             for (Connection c : list) {
